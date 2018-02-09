5de87f964db9a0b01ec14b243310ac2ac426e33c
kinglion580 <1609019405@qq.com>
2018-02-09 22:03:04 +0800

    finish

--- systemconn.sql
+++ systemconn.sql
@@ -1060,7 +1060,7 @@ drop profile pwd_time cascade;
 SHOW SERVEROUTPUT;
 SET SERVEROUTPUT ON;
 --无参
-CREATE PROCEDURE pro1
+CREATE OR REPLACE PROCEDURE pro1
 AS
 BEGIN
 	DBMS_OUTPUT.PUT_LINE('hello world');
@@ -1207,3 +1207,340 @@ drop table view_grade;
 drop procedure pro_grade_view;
 exec pro_grade_view;
 drop view view_grade;
+
+--使用游标
+set serveroutput on;
+show serveroutput;
+SELECT * FROM STUDENT;
+select * from course;
+select * from sc;
+
+CREATE OR REPLACE PROCEDURE PRO_SEX
+AS
+v_count NUMBER;
+v_sex student.s_sex%TYPE;
+CURSOR cur_sex
+IS
+SELECT s_sex FROM student GROUP BY s_sex;
+BEGIN
+OPEN cur_sex;
+
+LOOP
+FETCH cur_sex INTO v_sex;
+EXIT WHEN cur_sex%NOTFOUND;
+DBMS_OUTPUT.PUT_LINE('===================');
+DBMS_OUTPUT.PUT_LINE(v_sex || ' :');
+
+FOR row_student IN
+(
+  SELECT * FROM student WHERE s_sex=v_sex
+)
+LOOP
+  DBMS_OUTPUT.PUT_LINE(
+  'student id: ' || row_student.s_id ||
+  ' student name: '|| row_student.s_name ||
+  ' student age: ' || row_student.s_age
+  );
+END LOOP;
+
+SELECT count(s_id) INTO v_count FROM student WHERE s_sex=v_sex;
+IF SQL%FOUND THEN
+  DBMS_OUTPUT.PUT_LINE('count: ' || v_count);
+END IF;
+
+END LOOP;
+CLOSE cur_sex;
+END;
+/
+show errors procedure pro_sex;
+exec pro_sex;
+
+/*
+create or replace procedure pro_test
+as
+begin
+null;
+end;
+/
+drop procedure pro_test;
+*/
+
+
+CREATE OR REPLACE PROCEDURE PRO_SEX    --创建存储过程
+AS
+v_count NUMBER;         --变量，用于接收学生数量
+v_sex student.s_sex%TYPE;   --变量，用户接收学生性别
+CURSOR cur_sex      --创建游标
+IS
+SELECT s_sex FROM student GROUP BY s_sex;  --与游标关联的sql语句，此sql语句是查询出有哪些性别
+BEGIN
+OPEN cur_sex;      --打开游标
+
+--第一个循环开始，用来循环性别
+LOOP
+FETCH cur_sex INTO v_sex;    --获取游标当前指向的数据给变量 v_sex，第一次 v_sex 被赋值为 woman，第二次 v_sex 被赋值为 man
+EXIT WHEN cur_sex%NOTFOUND;    --当获取不到数据时退出
+DBMS_OUTPUT.PUT_LINE('===================');
+DBMS_OUTPUT.PUT_LINE(v_sex || ' :');
+
+--第二个循环开始，用来循环查询出的结果集，获取每行数据
+FOR row_student IN     --FOR 循环遍历结果中每行数据给变量 row_student 
+(
+  SELECT * FROM student WHERE s_sex=v_sex  --查询当前获取的性别有哪些学生
+)
+LOOP
+  DBMS_OUTPUT.PUT_LINE(
+  'student id: ' || row_student.s_id ||      --打印学生编号
+  ' student name: '|| row_student.s_name ||  --打印学生姓名
+  ' student age: ' || row_student.s_age      --打印学生年龄
+  );
+END LOOP;  --第二个循环结束
+
+SELECT count(s_id) INTO v_count FROM student WHERE s_sex=v_sex;   --查询出当前获取的性别的学生数量
+IF SQL%FOUND THEN     --如果此查询有结果就执行下面的语句
+  DBMS_OUTPUT.PUT_LINE('count: ' || v_count);   --打印数量
+END IF;
+
+END LOOP;  --第一个循环结束
+CLOSE cur_sex;    --关闭游标
+END;
+/
+
+--事务
+show autocommit;
+select * from student;
+select * from course;
+select * from sc;
+
+CREATE OR REPLACE PROCEDURE PRO_TRAN
+AS
+BEGIN
+  INSERT INTO student VALUES(1006,'shiyanlou1006','woman',22);
+  COMMIT;
+  INSERT INTO student VALUES(1007,'shiyanlou1007','woman',22);
+END;
+/
+exec pro_tran;
+select * from student;
+
+delete from student where s_id=1006;
+delete from student where s_id=1007;
+commit;
+
+CREATE OR REPLACE PROCEDURE PRO_TRAN
+AS
+BEGIN
+  INSERT INTO student VALUES(1006,'shiyanlou1006','woman',22);
+  INSERT INTO student VALUES(1006,'shiyanlou1007','woman',22);
+  COMMIT;
+EXCEPTION WHEN OTHERS THEN
+  rollback;
+END;
+/
+exec pro_tran;
+--保存点
+CREATE OR REPLACE PROCEDURE PRO_TRAN
+AS
+BEGIN
+  INSERT INTO student VALUES(1006,'shiyanlou1006','woman',22);
+  SAVEPOINT SP1;
+  INSERT INTO student VALUES(1007,'shiyanlou1007','woman',22);
+  SAVEPOINT SP2;
+  rollback to SP1;
+END;
+/
+exec pro_tran;
+
+show autocommit;
+set autocommit on;
+CREATE OR REPLACE PROCEDURE PRO_TRAN
+AS
+BEGIN
+  INSERT INTO student VALUES(1006,'shiyanlou1006','woman',22);
+  INSERT INTO student VALUES(1007,'shiyanlou1007','woman',22);
+END;
+/
+exec pro_tran;
+select * from student;
+
+delete from student where s_id=1006;
+delete from student where s_id=1007;
+commit;
+
+--删除存储过程
+drop procedure pro1;
+
+set autocommit off;
+delete from student where s_id=1006;
+commit;
+
+--备份恢复
+show parameter keep_time;
+
+create directory dpd as 'D:\app\shiyanlou\virtual\admin\orcl\dpdump';
+select * from dba_users where username like 'SYL%';
+select * from dba_directories where directory_name='DATA_PUMP_DIR';
+select * from dba_directories where directory_name='DPD';
+
+
+/*
+expdp system/Syl12345 full=y
+directory=dpd
+dumpfile=full_exp.dmp;
+*/
+
+alter user syl_imp identified by sylimp default tablespace SYSTEM;
+drop user syl_imp;
+select * from dba_users order by username;
+grant dba to syl_imp;
+grant read,write on directory dpd to syl_imp;
+/*
+$ expdp system/Syl12345 tables=student directory=dpd dumpfile=exp_student.dmp
+$ impdp system/Syl12345 remap_table=student:studentbak dumpfile=exp_student.dmp
+*/
+select * from tab;
+select * from tab where tname='STUDENTBAK';
+select * from studentbak;
+
+--RMAN
+alter system set nls_date_format='DD-MON-RR';
+alter system set nls_date_format='dd-mon-yyyy hh24:mi:ss' scope=spfile;
+alter system reset nls_date_format;
+select * from v$spparameter;
+select * from v$nls_parameters;
+
+$ export NLS_DATE_FORMAT='dd-mon-yyyy hh24:mi:ss'
+--存档模式
+select name,log_mode from v$database;
+select log_mode from v$database;
+select archiver from v$instance;
+select * from v$ARCHIVE_DEST;
+
+
+--backup incremental level=0 database plus archivelog;
+--configure controlfile autobackup on;
+
+--更改存档模式 ARCHIVELOG
+shutdown immediate;
+startup mount;
+alter database archivelog;
+alter database open;
+
+select name,value from v$parameter where name like 'db_recovery%';
+select dest_name,destination from v$ARCHIVE_DEST where dest_name='LOG_ARCHIVE_DEST_1';
+
+RMAN> set echo on;
+RMAN> configure retention policy to recovery window of 3 days;
+RMAN> configure device type disk parallelism 2 backup type to compressed backupset;
+RMAN> configure channel 1 device type disk format '/u01/backup1';
+RMAN> configure channel 2 device type disk format '/u01/backup2';
+RMAN> configure archivelog deletion policy to backed up 2 times to disk;
+
+RMAN> backup database;
+
+select * from dba_tablespaces;
+select name from v$datafile;
+RMAN> configure device type disk clear;
+RMAN> configure channel 1 device type disk clear;
+RMAN> configure channel 2 device type disk clear;
+RMAN> configure channel device type disk format '/u01/rman_%U.bak';
+RMAN> backup tablespace syltp1;
+--增量备份
+RMAN> backup incremental level 0 tablespace syltp1;
+RMAN> backup incremental level 1 tablespace syltp1;
+
+--恢复
+--完全
+RMAN> restore tablespace syltp1 preview summary;
+RMAN> restore tablespace syltp1 validate;
+RMAN> alter tablespace syltp1 offline immediate;
+RMAN> restore tablespace syltp1;
+RMAN> recover tablespace syltp1;
+RMAN> alter tablespace syltp1 online;
+
+--不完全
+RMAN> select current_scn from v$database;
+RMAN> select * from tab where tname='SYL_RES';
+RMAN> create table syl_res (id number);
+RMAN> create restore point syl_res_scn;
+select file#,status,fuzzy,error,checkpoint_change#,to_char(checkpoint_time,'dd-mon-rrrr hh24:mi:ss') as checkpoint_time from v$datafile_header;
+RMAN> select name,scn from v$restore_point;
+select name,value from v$parameter where name='control_file_record_keep_time';
+RMAN> drop table syl_res;
+RMAN> select * from tab where tname='syl_res';
+RMAN> shutdown immediate;
+RMAN> startup mount;
+RMAN> restore database until restore point syl_res_scn;
+RMAN> recover database until restore point syl_res_scn;
+RMAN> alter database open resetlogs;
+RMAN> select * from tab where tname='SYL_RES';
+
+--闪回
+select log_mode from v$database;
+archive log list;
+select * from v$parameter where name like 'db_recovery_file_dest%';
+show parameter db_recovery_file_dest;
+alter system set db_recovery_file_dest_size=5g;
+alter system set db_recovery_file_dest='D:\app\flashback';
+show parameter db_flashback_retention_target;
+alter database flashback on;
+--alter database flashback off;
+select flashback_on from v$database;
+--查询闪回日志
+select name,log#,thread#,sequence#,bytes from v$flashback_database_logfile;
+--闪回时间范围
+select oldest_flashback_scn,to_char(oldest_flashback_time,'dd-mon-yyyy hh24:mi:ss') from v$flashback_database_log;
+--打开数据库
+--alter database open;
+desc v$flashback_database_log;
+select retention_target,flashback_size,oldest_flashback_time from v$flashback_database_log;
+select end_time,flashback_data,db_data,redo_data from v$flashback_database_stat;
+select * from v$sgastat where name='flashback generation buff';
+--使用闪回
+select * from tab where tname='SYL_RES';
+create restore point flash_syl_res;
+drop table syl_res;
+shutdown immediate;
+startup mount;
+select name,scn from v$restore_point;
+flashback database to restore point flash_syl_res;
+alter database open resetlogs;
+
+--闪回表
+alter database flashback off;
+select * from dba_tablespaces;
+select username,default_tablespace from dba_users;
+create user sylflash identified by sylflash
+default tablespace SYLTP1;
+grant dba to sylflash;
+select * from dba_role_privs where grantee='SYLFLASH';
+--select * from dba_sys_privs where grantee='SYLFLASH';
+--select * from dba_tab_privs where grantee='SYLFLASH';
+
+show parameter recyclebin;
+create table test(id number);
+insert into test values(1);
+insert into test values(2);
+insert into test values(3);
+select * from test;
+drop table test;
+select * from tab;
+show recyclebin;
+select * from user_recyclebin;
+select object_name,original_name,type from recyclebin;
+flashback table test to before drop;
+select index_name from user_indexes where table_name='TEST';
+
+select current_scn from v$database; 2180338
+select table_name,row_movement from user_tables;
+alter table test enable row movement;
+delete from test where id=3;
+select * from test;
+flashback table test to scn 2180338;
+select * from test;
+
+purge recyclebin;
+purge dba_recyclebin;
+drop table test purge;
+
+
